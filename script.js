c2b7424d0203d6e3a82a0421348bd76145553f3d
Turpal <salangeriev97@mail.ru>
2023-08-29 23:58:05 +0300

    the first project

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+let weight = prompt('Enter your weight in kilos');
+let Height = prompt('Enter your height in meters');
+let BMI = weight / Math.pow(Height,2);
+console.log(BMI);
+let t = 25 - BMI;
+if (BMI >= 25) {
+    alert('You are overweight more than by ' + t)
+} else {
+    alert('You are normal')
+}
+*/
+/*
+0, undefined, null, NaN, ''*/ // falsy values
